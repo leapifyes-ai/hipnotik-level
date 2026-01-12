a050c6795ad82945d455a981a8a1c697b3f69a16
Ricardo Serrano <leapifyes@gmail.com>
2026-01-12 17:32:13 +0000

    auto-commit for 6b817d4c-5ea7-435d-91ae-b395bc3577cc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import Contacts from './pages/Contacts';
 import Calculator from './pages/Calculator';
 import Reports from './pages/Reports';
 import Commissions from './pages/Commissions';
+import Help from './pages/Help';
 import Settings from './pages/Settings';
 
 const PrivateRoute = ({ children }) => {
